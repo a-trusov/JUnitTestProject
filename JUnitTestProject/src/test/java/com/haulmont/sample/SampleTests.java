56d36e23204809e2b2f5c8b96a60e426f3d2e7d6
Andrey Trusov <trusov@haulmont.com>
2020-02-11 18:47:42 +0400

    mock sender and repository

---
+++ JUnitTestProject/src/test/java/com/haulmont/sample/SampleTests.java
@@ -0,0 +1,41 @@
+package com.haulmont.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SampleTests {
+
+    @Test
+    public void iterator_will_return_hello_world() {
+        Iterator i = mock(Iterator.class);
+
+        when(i.next()).thenReturn("Hello").thenReturn("World");
+
+        String result = i.next() + " " + i.next();
+
+        assertEquals("Hello World", result);
+    }
+
+    @Test
+    public void whenNotUseMockAnnotation_thenCorrect() {
+        List mockList = Mockito.mock(ArrayList.class);
+
+        mockList.add("one");
+        Mockito.verify(mockList).add("one");
+        assertEquals(0, mockList.size());
+
+        Mockito.when(mockList.size()).thenReturn(100);
+        assertEquals(100, mockList.size());
+    }
+}
--- JUnitTestProject/src/test/java/com/haulmont/task/MailServiceTest.java
+++ JUnitTestProject/src/test/java/com/haulmont/task/MailServiceTest.java
@@ -1,52 +1,85 @@
 package com.haulmont.task;
 
+import com.haulmont.example.order.Order;
 import com.haulmont.example.order.OrderStatus;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.*;
 
 import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.given;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
-@RunWith(MockitoJUnitRunner.class)
 public class MailServiceTest {
+
+    // тестируемый сервис
+    private MailService mailService;
 
     @Mock
     private MailSender mailSender;
-    @Mock
+    @Spy
     private MessageRepository messageRepository;
-    @Mock
-    private MailService mailService;
+    @Spy
+    private Person from;
+    @Spy
+    private Person to;
+
+    private Message message;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         this.mailService = new MailService(mailSender, messageRepository);
+//        Mockito.doAnswer(invocation -> {
+//            Person from = invocation.getArgument(0);
+//            from.setEmail("from@mail");
+//
+//            Person to = invocation.getArgument(0);
+//            to.setEmail("to@mail");
+//
+//            return message;
+//        }).when(messageRepository).addMessage(any(),any(), anyString(), anyString());
+        // не придумал как замокать без прописывания email, через doAnswer не вышло
+        from.setEmail("from@mail");
+        to.setEmail("to@mail");
+        message = messageRepository.addMessage(from, to, "Subject", "Message");
     }
 
     @Test
-    public void sendMessageShouldReturnTrue() {
-        Message message = messageRepository.addMessage(any(), any(), anyString(), anyString());
-        Mockito.when(mailService.sendMessage(message)).thenReturn(true);
-        boolean exists = mailService.sendMessage(message);
-        assertTrue(exists);
+    public void sendMessageShouldReturnDelivered() {
+        Mockito.when(mailSender.sendMessage(anyString(), anyString(), anyString(), anyString())).thenReturn(true);
+        boolean result = mailService.sendMessage(message);
+
+        assertTrue(result);
         assertEquals(message.getStatus(), Status.DELIVERED);
     }
 
-//    @Test
-//    public void sendMessageShouldReturnFalse() {
-//    }
-//
-//    @Test
-//    public void removeMessage() {
-//    }
+    @Test
+    public void sendMessageShouldReturnError() {
+        Mockito.when(mailSender.sendMessage(anyString(), anyString(), anyString(), anyString())).thenReturn(false);
+        boolean result = mailService.sendMessage(message);
+        assertFalse(result);
+        assertEquals(message.getStatus(), Status.ERROR);
+    }
+
+    @Test
+    public void testMethodCalled() {
+        message.setStatus(Status.DELIVERED);
+        mailService.sendMessage(message);
+        Mockito.verify(mailSender, Mockito.times(1)).sendMessage(anyString(), anyString(), anyString(), anyString());
+    }
+
+
+    //тест падает, не понял почему...
+    @Test
+    public void testMethodNeverCalled() {
+        message.setStatus(Status.NEW);
+        mailService.sendMessage(message);
+        Mockito.verify(mailSender, Mockito.never()).sendMessage(anyString(), anyString(), anyString(), anyString());
+    }
+
+
+
 }
 
 //Необходимо покрыть тестами класс com.haulmont.task.MailService
